4d23705d7ce3031de2f7e7d430dfe9cff582ebff
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-18 13:36:34 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,13 @@
-
+def calculator(sqrt,floor):
+    ''' function calculator takes 2 parameters
+        and returns sqrt and floor and takes into account
+        certain parameter limitations'''
+    if sqrt or floor >0:
+      import math
+      try:
+          solution=(round(math.sqrt(sqrt),2),math.floor(floor))
+          return "The squared root returns {} and floor returns {}".format(solution[0],solution[1])
+      except:
+           return "Must be a poisitive real number"
+    else:
+          return "Gotta be greater than 0"
