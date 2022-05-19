8c080cb30fc657b4142cf4ae007763879a7d7f41
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-18 21:16:33 -0500

    Update playing-craps.py

--- playing-craps.py
+++ playing-craps.py
@@ -1,1 +1,7 @@
-
+# importing necessary modules
+from collections import Counter
+import numpy as np
+random_dice_roll = np.random.randint(1,7,size=6_000_000)
+results= Counter(random_dice_roll)
+for key,value in sorted(results.items()):
+    print('Face: {} Frequency: {}'.format(key,value))
