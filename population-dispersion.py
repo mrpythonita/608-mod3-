a2e884ea9fcf780080f41d0aa70b7b4002f36808
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-19 06:48:33 -0500

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,8 @@
-
+# Using the statistics module to calculate 
+# measures of dispersion for a population. 
+import statistics
+import numpy as np
+data = np.random.randint(1,7,size=6_000_000)
+population_variance = statistics.pvariance(data)
+population_std_deviation = statistics.pstdev(data)
+print('The population variance for the dataset assigned is {} and population standard deviation is {}.'.format(population_variance,population_std_deviation))
