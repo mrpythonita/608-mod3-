459910e4a95c9aa2be5b4acec5e8c12064db8fa4
Pasquale Salomone <33000028+mrpythonita@users.noreply.github.com>
2022-05-19 19:41:47 -0500

    Update bult-in-min-max.py

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -15,4 +15,4 @@ def max_min_value(*args):
         return "Please only enter intger/float numbers"
     return "The max value is {} and the min value is {}.".format(result[0],result[1])
 
-max_min_value(12,23,34,45,67)
+print(max_min_value(12,23,34,45,67))
